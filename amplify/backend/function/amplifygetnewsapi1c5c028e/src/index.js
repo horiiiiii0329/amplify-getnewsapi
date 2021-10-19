72303d77be0239a8593bcb94dd096e50302b4599
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-10-19 21:23:44 +0900

    initial setup for lambda function

---
+++ amplify/backend/function/amplifygetnewsapi1c5c028e/src/index.js
@@ -0,0 +1,19 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	FUNCTION_AMPLIFYGETNEWSAPI05E3C7FE_NAME
+	REGION
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+  // TODO implement
+  const response = {
+    statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
+};
